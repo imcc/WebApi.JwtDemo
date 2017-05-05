ebbac57a0e3f21c0ac137cb7337a3584d937706e
明尘 <imcc.beta@gmail.com>
2017-05-05 10:32:17 +0800

    添加项目文件。

---
+++ WebApi.Jwt/Controllers/ValueController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApi.Jwt.Filters;
+
+namespace WebApi.Jwt.Controllers
+{
+    [JwtAuthentication]
+    public class ValueController : ApiController
+    {
+        public string Get()
+        {
+            var username = HttpContext.Current.User.Identity.Name;
+
+            return username;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public string Get(int id)
+        {
+            return $"{id}\t{HttpContext.Current.User.Identity.Name}";
+        }
+    }
+}
